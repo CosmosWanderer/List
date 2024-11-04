ffc0d78b70c769f8f90a35c492e4578d8c3548f9
Maxim <scourgetp@gmail.com>
2024-11-04 23:31:07 +0300

    Added Iterator

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -165,3 +165,147 @@ TEST(List, can_find_element) {
 	EXPECT_EQ(l.find(10), &l[3]);
 	EXPECT_EQ(l.find(9), nullptr);
 }
+
+
+// Тесты для итератора
+
+TEST(Iterator, can_create_iterator) {
+	List<int> l(5);
+
+	EXPECT_NO_THROW(List<int>::Iterator it = l.begin());
+}
+
+TEST(Iterator, begin_gives_right_iterator) {
+	List<int> l(5);
+
+	l[0].value = 1;
+	
+	EXPECT_EQ(*l.begin(), 1);
+}
+
+TEST(Iterator, iterators_are_equal_if_they_are_equal) {
+	List<int> l(5);
+
+	auto it1 = l.begin();
+	auto it2 = l.begin();
+	
+	EXPECT_TRUE(it1 == it2);
+}
+
+TEST(Iterator, iterators_are_not_equal_if_they_are_not_equal) {
+	List<int> l(5);
+	
+	auto it1 = l.begin();
+	auto it2 = l.begin();
+	++it2;
+	
+	EXPECT_TRUE(it1 != it2);
+}
+
+TEST(Iterator, can_increment_iterator_prefix) {
+	List<int> l(3);
+	
+	l[0].value = 1;
+	l[1].value = 2;
+	l[2].value = 3;
+	auto it = l.begin();
+	++it;
+	
+	EXPECT_EQ(*it, 2);
+}
+
+TEST(Iterator, can_increment_iterator_postfix) {
+	List<int> l(3);
+	
+	l[0].value = 1;
+	l[1].value = 2;
+	l[2].value = 3;
+	auto it = l.begin();
+	auto it2 = it++;
+	
+	EXPECT_EQ(*it2, 1);
+	EXPECT_EQ(*it, 2);
+}
+
+TEST(Iterator, can_get_value_from_iterator) {
+	List<int> l(5);
+
+	l[2].value = 42;
+	auto it = l.begin();
+	it += 2;
+
+	EXPECT_EQ(*it, 42);
+}
+
+TEST(Iterator, cant_get_value_out_of_range) {
+	List<int> l(3);
+
+	auto it = l.begin();
+	++it; 
+	++it; 
+	++it; // Находимся в конце листа
+	
+	EXPECT_ANY_THROW(++it);
+}
+
+TEST(Iterator, cant_get_not_existing_value) {
+	List<int> l(3);
+
+	auto it = l.end();
+	
+	EXPECT_ANY_THROW(*it);
+}
+
+TEST(Iterator, can_use_operator_plus) {
+	List<int> l(5);
+	
+	l[4].value = 50;
+	auto it = l.begin();
+	it = it + 4;
+	
+	EXPECT_EQ(*it, 50);
+}
+
+TEST(Iterator, cant_use_plus_operator_out_of_range) {
+	List<int> l(3);
+	
+	auto it = l.begin();
+	
+	EXPECT_ANY_THROW(it + 4);
+}
+
+TEST(Iterator, can_use_operator_plus_equal) {
+	List<int> l(5);
+	
+	l[3].value = 30;
+	auto it = l.begin();
+	it += 3;
+	
+	EXPECT_EQ(*it, 30);
+}
+
+TEST(Iterator, cant_use_operator_plus_equal_out_of_range) {
+	List<int> l(3);
+	
+	auto it = l.begin();
+	
+	EXPECT_ANY_THROW(it += 4, std::out_of_range);
+}
+
+TEST(Iterator, can_go_throughout_the_list) {
+	List<int> l(10);
+
+	for (int i = 0; i < 10; i++) {
+		l[i].value = i;
+	}
+
+	bool isEqual = true;
+
+	int rightValue = 0;
+	for (List<int>::Iterator it = l.begin(); it != l.end(); it++) {
+		if (*it != rightValue) isEqual = false;
+		rightValue++;
+	}
+
+	EXPECT_TRUE(isEqual);
+}
